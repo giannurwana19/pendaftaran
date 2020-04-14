02c13ce0e0efc51de13944355d2d8510c40f06d0
giannurwana19 <giannurwana19@gmail.com>
2020-04-14 18:55:14 +0700

    menambahkan fitur edit dan upload gambar

--- siswa/editprofil.php
+++ siswa/editprofil.php
@@ -7,10 +7,9 @@
 
   <!-- Page Heading -->
   <h1 class="h3 mb-4 text-gray-800">EDIT PROFILE</h1>
-
-  <div class="row">
-    <div class="col-md-8">
-      <form class="user" method="POST" action="<?= $base_url; ?>/siswa/editprofile.php">
+  <form class="user" method="POST" action="<?= $base_url; ?>/siswa/editprofil.php" enctype="multipart/form-data">
+    <div class="row">
+      <div class="col-md-8">
         <div class="form-group">
           <label for="nama">Nama</label>
           <input type="text" name="nama" class="form-control" id="nama" value="<?= $data_pendaftar['nama']; ?>" placeholder="nama anda">
@@ -39,11 +38,11 @@
 
             ?>
             <div class="form-check">
-              <input type="radio" name="jk" class="form-check-input" id="laki" value="laki-laki" <?= $laki; ?>>
+              <input type="radio" name="jenis_kelamin" class="form-check-input" id="laki" value="L" <?= $laki; ?>>
               <label for="laki">Laki-Laki</label>
             </div>
             <div class="form-check">
-              <input type="radio" name="jk" class="form-check-input" id="perempuan" value="perempuan" <?= $perempuan; ?>>
+              <input type="radio" name="jenis_kelamin" class="form-check-input" id="perempuan" value="P" <?= $perempuan; ?>>
               <label for="perempuan">perempuan</label>
             </div>
           </div>
@@ -95,22 +94,24 @@
             <input type="password" name="password2" id="password2" class="form-control" placeholder="password">
           </div>
         </div> -->
-        <button type="submit" class="btn btn-primary mb-5" name="btn_simpan" value="simpan_profil">Ubah</button>
+      </div>
+      <div class="col-md-4">
+        <?php
+        if ($data_pendaftar['foto'] == null) {
+          $foto = '../assets/img/1.png';
+        } else {
+          $foto = '../upload/' . $data_pendaftar['foto'];
+        }
+        ?>
+        <img src="<?= $foto; ?>" alt="foto profil" class="img-fluid">
+        <input type="file" name="foto" class="form-control mt-2">
+      </div>
+      <div class="col-md-12">
+        <button type="submit" class="btn btn-success mb-5" name="btn_simpan" value="simpan_profil">Ubah</button>
         <a href="dashboard.php" class="btn btn-danger mb-5">kembali</a>
-      </form>
+      </div>
     </div>
-    <div class="col-md-4">
-      <?php
-      if ($data_pendaftar['foto'] == null) {
-        $foto = '../assets/img/1.png';
-      } else {
-        $foto = '../upload/' . $data_pendaftar['foto'];
-      }
-      ?>
-      <img src="<?= $foto; ?>" alt="foto profil" class="img-fluid">
-      <input type="file" name="gambar" class="form-control mt-2">
-    </div>
-  </div>
+  </form>
 
 </div>
 <!-- /.container-fluid -->
--- siswa/editprofil_control.php
+++ siswa/editprofil_control.php
@@ -6,6 +6,77 @@ $result_pendaftar = mysqli_query($koneksi, $sql_pendaftar);
 
 if(mysqli_num_rows($result_pendaftar)){
   $data_pendaftar = mysqli_fetch_array($result_pendaftar);
+
+  if(isset($_POST['btn_simpan']) && $_POST['btn_simpan'] == "simpan_profil"){
+    // echo "update";
+    // die;
+    $id_pendaftar = $data_pendaftar['id'];
+    $nama = $_POST['nama'];
+    $tmpt_lahir = $_POST['tmpt_lahir'];
+    $tgl_lahir = date('Y-m-d', strtotime($_POST['tgl_lahir']));
+    $jenis_kelamin = $_POST['jenis_kelamin'];
+    $agama = $_POST['agama'];
+    $alamat = $_POST['alamat'];
+    $email = $_POST['email'];
+    $telepon = $_POST['telepon'];
+
+    // cek gambar apakah diupload atau tidak
+    if($_FILES['foto']['name'] != null){
+      $ekstensi_boleh = ['png','jpg'];
+      $nama_gambar = $_FILES['foto']['name'];
+      $x = explode('.',$nama_gambar);
+      $ekstensi = strtolower(end($x));
+      $ukuran = $_FILES['foto']['size'];
+      $file_tmp = $_FILES['foto']['tmp_name'];
+      
+      $ubah_nama = $nama . "." . $ekstensi;
+
+      // echo $ubah_nama;
+      // die;
+
+      if(in_array($ekstensi, $ekstensi_boleh) === true){
+        if($ukuran < 1000000){
+          move_uploaded_file($file_tmp, '../upload/' . $ubah_nama);
+
+          $sql_update_gambar = "UPDATE pendaftar SET foto = '$ubah_nama' WHERE id='$id_pendaftar'";
+          $query_update = mysqli_query($koneksi, $sql_update_gambar);
+
+          if($query_update){
+
+          }else{
+            echo "gagal upload"; die;
+          }
+
+        }else{
+          echo "gambar terlalu besar"; die;
+        }
+      }else{
+        echo "ekstensi tidak diperbolehkan"; die;
+      }
+
+    }
+
+    $sql_update_profil = "UPDATE pendaftar SET
+                                  nama = '$nama',
+                                  tmpt_lahir = '$tmpt_lahir',
+                                  tgl_lahir = '$tgl_lahir',
+                                  jenis_kelamin = '$jenis_kelamin',
+                                  agama = '$agama',
+                                  alamat = '$alamat',
+                                  email = '$email',
+                                  telepon = '$telepon'
+                          WHERE id = '$id_pendaftar'";
+                          
+    $query_update_profile = mysqli_query($koneksi, $sql_update_profil);
+
+    if($query_update_profile){
+      header("Location: dashboard.php");
+    }else{
+      echo "gagal update data";
+      die;
+    }
+
+  }
 }
 
 
